7284ba265f6f51485b562f8b7ee4bcdbe9239a2a
Jeff <farlandjeff@gmail.com>
2022-11-01 19:34:12 -0400

    Add nav bar functionality

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
 const landingBtn = document.querySelector('.landingbtn');
+const homeNav = document.querySelector('.home-link');
+const aboutNav = document.querySelector('.about-link');
+const projectsNav = document.querySelector('.projects-link');
+const contactNav = document.querySelector('.contact-link');
+
+// Landing Page Button
 landingBtn.addEventListener('click', () => {
     document.querySelector('.navBar').scrollIntoView();
+});
+
+// Nav Bar Links
+homeNav.addEventListener('click', () => {
+    document.querySelector('.landing').scrollIntoView();
+});
+
+aboutNav.addEventListener('click', () => {
+    document.querySelector('.navBar').scrollIntoView();
+});
+
+projectsNav.addEventListener('click', () => {
+    document.querySelector('#projects').scrollIntoView();
+});
+
+contactNav.addEventListener('click', () => {
+    document.querySelector('#contact').scrollIntoView();
 });
 
